917fb06a2d7f2c29cb23b4ce93ea091e5567492e
Josh Kearl <Josh.kearl@gmail.com>
2018-07-25 00:46:50 -0600

    added totaling, and descriptions

--- src/app/scorecard/scorecard.component.ts
+++ src/app/scorecard/scorecard.component.ts
@@ -17,28 +17,27 @@ export interface ScoreCard {
   hcp: number;
   par: number;
   overUnder?: number;
-  totalScore?: number;
 }
 
 const ELEMENT_DATA: ScoreCard[] = [
-  {holeNumber: 1, blank: '', score: 3, yardage: 1.0079, hcp: 3, par: 4,},
-  {holeNumber: 2, blank:'', score: 3, yardage: 4.0026, hcp: 3, par: 3,},
-  {holeNumber: 3, blank:'', score: 3, yardage: 6.941, hcp: 3 , par: 4,},
-  {holeNumber: 4, blank:'', score: 4, yardage: 9.0122, hcp: 3, par: 5,},
-  {holeNumber: 5, blank:'', score: 5, yardage: 10.811, hcp: 3, par: 4,},
-  {holeNumber: 6, blank:'', score: 3, yardage: 12.0107, hcp: 3, par: 3,},
-  {holeNumber: 7, blank:'', score: 7, yardage: 14.0067, hcp: 3,par: 3,},
-  {holeNumber: 8, blank:'', score: 6, yardage: 15.9994, hcp: 3,par: 3,},
-  {holeNumber: 9, blank:'', score: 8, yardage: 18.9984, hcp: 3,par: 4,},
-  {holeNumber: 10, blank:'', score: 5, yardage: 20.1797, hcp: 3,par: 5,},
-  {holeNumber: 11, blank:'', score: 5, yardage: 20.1797, hcp: 3,par: 5,},
-  {holeNumber: 12, blank:'', score: 4, yardage: 20.1797, hcp: 3,par: 2,},
-  {holeNumber: 13, blank:'', score: 3, yardage: 20.1797, hcp: 3,par: 4,},
-  {holeNumber: 14, blank:'', score: 6, yardage: 20.1797, hcp: 3,par: 3,},
-  {holeNumber: 15, blank:'', score: 7, yardage: 20.1797, hcp: 3,par: 3,},
-  {holeNumber: 16, blank:'', score: 8, yardage: 20.1797, hcp: 3,par: 5,},
-  {holeNumber: 17, blank:'', score: 7, yardage: 20.1797, hcp: 3,par: 4,},
-  {holeNumber: 18, blank:'', score: 4, yardage: 20.1797, hcp: 3,par: 3,},
+  {holeNumber: 1, score: 3, yardage: 1.0079, hcp: 3, par: 4,},
+  {holeNumber: 2,  score: 3, yardage: 4.0026, hcp: 3, par: 3,},
+  {holeNumber: 3,  score: 3, yardage: 6.941, hcp: 3 , par: 4,},
+  {holeNumber: 4,  score: 4, yardage: 9.0122, hcp: 3, par: 5,},
+  {holeNumber: 5,  score: 5, yardage: 10.811, hcp: 3, par: 4,},
+  {holeNumber: 6,  score: 3, yardage: 12.0107, hcp: 3, par: 3,},
+  {holeNumber: 7,  score: 7, yardage: 14.0067, hcp: 3,par: 3,},
+  {holeNumber: 8,  score: 6, yardage: 15.9994, hcp: 3,par: 3,},
+  {holeNumber: 9,  score: 8, yardage: 18.9984, hcp: 3,par: 4,},
+  {holeNumber: 10,  score: 5, yardage: 20.1797, hcp: 3,par: 5,},
+  {holeNumber: 11,  score: 5, yardage: 20.1797, hcp: 3,par: 5,},
+  {holeNumber: 12,  score: 4, yardage: 20.1797, hcp: 3,par: 2,},
+  {holeNumber: 13,  score: 3, yardage: 20.1797, hcp: 3,par: 4,},
+  {holeNumber: 14,  score: 6, yardage: 20.1797, hcp: 3,par: 3,},
+  {holeNumber: 15,  score: 7, yardage: 20.1797, hcp: 3,par: 3,},
+  {holeNumber: 16,  score: 8, yardage: 20.1797, hcp: 3,par: 5,},
+  {holeNumber: 17,  score: 7, yardage: 20.1797, hcp: 3,par: 4,},
+  {holeNumber: 18,  score: 4, yardage: 20.1797, hcp: 3,par: 3,},
 ];
 
 
@@ -49,7 +48,8 @@ const ELEMENT_DATA: ScoreCard[] = [
   styleUrls: ['./scorecard.component.css']
 })
 export class ScorecardComponent implements OnInit {
-  displayedColumns: string[] = ['holeNumber',];
+  displayedColumns: string[] = ['holeNumber', 'score', 'yardage', 'hcp', 'par',];
+  displayedFooters: string[] = ['holeNumber', 'score',];
   dataSource = ELEMENT_DATA;
 
   activeCourse;
@@ -64,5 +64,8 @@ export class ScorecardComponent implements OnInit {
     this.teeService.activeTee.subscribe(activeTee => {
       this.activeTee = activeTee;
     });
+  }
+  getTotal() {
+    return this.dataSource.map(t => t.score).reduce((acc, value) => acc + value, 0);
   }
 }
